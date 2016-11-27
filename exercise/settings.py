0c6acf1f455beca2486e3a184fc3c98d3dcb379a
bluedai180 <bluedai180@gmail.com>
2016-11-27 23:52:15 +0800

    Init project

---
+++ exercise/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for exercise project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'exercise'
+BOT_VERSION = '1.0'
+
+SPIDER_MODULES = ['exercise.spiders']
+NEWSPIDER_MODULE = 'exercise.spiders'
+USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+
