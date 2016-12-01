ba19961e266ec80a11e433aa6e77dee91e4495a3
bluedai180 <bluedai180@gmail.com>
2016-12-02 00:24:59 +0800

    Change the source website

--- exercise/spiders/jirou.py
+++ exercise/spiders/jirou.py
@@ -5,21 +5,21 @@ from exercise.items import ExerciseItem
 
 class DmozSpider(scrapy.Spider):
     name = "exercise"
-    allowed_domains = ["jirou.com"]
+    allowed_domains = ["hiyd.com"]
     start_urls = [
-        "http://www.jirou.com/"
+        "http://www.hiyd.com/play/course?id=38"
     ]
 
     def parse(self, response):
-        sites = response.xpath('//div[@class="content6"]')
+        sites = response.xpath('//div[@class="info"]')
         items = []
         for site in sites:
             item = ExerciseItem()
-            title = site.xpath('a/text()').extract()
-            link = site.xpath('a/@href').extract()
+            title = site.xpath('span/text()').extract()
+            #link = site.xpath('avatar/@href').extract()
 
             item['title'] = [t.encode('utf-8') for t in title]
-            item['link'] = [l.encode('utf-8') for l in link]
+            #item['link'] = [l.encode('utf-8') for l in link]
 
             items.append(item)
         return items
--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 from scrapy import cmdline
 cmdline.execute("scrapy crawl exercise".split())
+#cmdline.execute("scrapy crawl exercise -o items.json".split())
